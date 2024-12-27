2bf8d3c1a7c441e64e8ff1d4b190b1a40428650b
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-12-27 22:40:47 +0300

    Created Project

---
+++ Volatus/Volatus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Volatus
+//
+//  Created by Engin Gülek on 27.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Volatus/Volatus/VolatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolatusApp.swift
+//  Volatus
+//
+//  Created by Engin Gülek on 27.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct VolatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
